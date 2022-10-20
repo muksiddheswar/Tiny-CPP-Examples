e59594560ab85c2a5a7af1f4909e75944b72b530
konkeror <sid.mkjee@gmail.com>
2022-10-20 08:27:28 +0200

    Replace and sizeof example

--- CPP-Examples/STL/strings/string_replace_and_sizeof.cpp
+++ CPP-Examples/STL/strings/string_replace_and_sizeof.cpp
@@ -10,22 +10,25 @@ int main()
     string s3 = "GeeksforGeeks";
     string s4 = "HeyWorld !";
  
+    cout << s1 << endl;
     cout << "Sizeof - " << sizeof(s1) << endl;
     cout << "Capacity - " << s1.capacity() << endl;
     cout << "Size - " <<s1.size() << endl;
     // cout << s1.max_size() << endl;
 
     s1.push_back('X');
+    cout << endl << s1 << endl;
 
-    cout << endl<< "Sizeof - " << sizeof(s1) << endl;
+    cout << "Sizeof - " << sizeof(s1) << endl;
     cout << "Capacity - " << s1.capacity() << endl;
     cout << "Size - " <<s1.size() << endl;    
-
-
-
     
-    // s1.replace(0, 7, s2);
-    // cout << s1 << endl;
+    s1.replace(0, 7, s2);
+    cout << endl << s1 << endl;
+
+    cout << "Sizeof - " << sizeof(s1) << endl;
+    cout << "Capacity - " << s1.capacity() << endl;
+    cout << "Size - " <<s1.size() << endl;    
  
     return 0;
 }
